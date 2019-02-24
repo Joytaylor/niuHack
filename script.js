15f44803ff04ef9f738ba0579c0a653023cb486b
Joy <jtaylor2@imsa.edu>
2019-02-24 08:59:27 -0600

    cleaning script

---
+++ script.js
@@ -0,0 +1,40 @@
+var message=[];
+  var database = firebase.firestore(app);
+
+  database.collection("quotes").get()
+  .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            message.push(doc.data());
+        });
+        pickMessage();
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+  function pickMessage(){
+    var word= "";
+    var i = 0;
+    var pastNum = [];
+    while(i<3){
+      var tempNum = Math.floor(Math.random()*message.length);
+  if( pastNum.find(function(element) {
+  return element == tempNum;
+
+})!=null){
+
+}
+else{
+  $("#goodQuote"+word).text(message[tempNum].quote);
+  pastNum.push(tempNum);
+
+
+  word++;
+  i++;
+}
+
+    }
+
+
+  }
